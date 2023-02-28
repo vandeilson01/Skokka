3730b5468dc9979f1490924883324c4faad26000
kelvin diego <kelvin.fabiana@gmail.com>
2023-02-28 15:47:09 -0300

    Add states and category filter sellect

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,3 +1,7 @@
+@php
+$categories = App\Models\Category::all();
+$states = App\Models\State::all();
+@endphp
 <nav x-data="{ open: false, search:false }" class="bg-zinc-100 border-b border-gray-100 p-10 flex flex-col justify-between items-center space-y-10">
     <div class="container flex md:flex-col lg:flex-row justify-between items-center w-full mx-auto">
             <a href="/" class="w-64">
@@ -54,24 +58,27 @@
         <div class="container md:flex flex-col space-y-9 items-center justify-between w-full">
         <div class="flex items-center justify-between space-x-5 w-full">
            <select name="categories" class="block border-zinc-200  font-medium text-gray-400 rounded w-1/2">
-               <option>Acompanhantes</option>
+
+            @foreach ($categories as $categorie)
+            <option class="{{ $categorie->id }}">{{ $categorie->name }}</option>
+            @endforeach
+              
            </select>
            <input type="text" class="block border-zinc-200 font-medium rounded w-1/2" placeholder="pesquise aqui..." name="terms">
         </div>
          
         
-        <div class="flex items-center justify-between space-x-5 w-full">
+        <div class="flex items-center justify-center space-x-5 w-full">
            <select name="state" class="block border-zinc-200  font-medium text-gray-400 rounded w-1/2">
-               <option>Sao paullo</option>
-           </select>
-           <select name="city" class="block border-zinc-200 font-medium text-gray-400 rounded w-1/2">
-               <option>Sao paulo</option>
+            @foreach ($states as $state)
+            <option class="{{ $state->id }}">{{ $state->name }}</option>
+           @endforeach
            </select>
            <button type="submit" class="flex items-center px-10  bg-shokka-pink text-white font-normal  rounded border-none transition ease-in hover:bg-shokka-blue-hover">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
-             <span>
+             <span class="py-3">
                 PESQUISAR
              </span>
            </button>
@@ -84,7 +91,9 @@
       <div class="container md:flex flex-row space-y-9 items-center justify-between w-full">
       <div class="flex flex-col items-center justify-between space-y-5 w-full">
          <select name="categories" class="border-zinc-200  font-medium text-gray-400 rounded w-full">
-             <option>Acompanhantes</option>
+          @foreach ($categories as $categorie)
+          <option class="{{ $categorie->id }}">{{ $categorie->name }}</option>
+          @endforeach
          </select>
          <input type="text" class="border-zinc-200 font-medium rounded w-full" placeholder="pesquise aqui..." name="terms">
       </div>
@@ -92,16 +101,15 @@
       
       <div class="flex flex-col items-center justify-between space-y-5 w-full">
          <select name="state" class=" border-zinc-200  font-medium text-gray-400 rounded w-full">
-             <option>Sao paullo</option>
-         </select>
-         <select name="city" class=" border-zinc-200 font-medium text-gray-400 rounded w-full">
-             <option value="sao paulo">Sao paulo</option>
+          @foreach ($states as $state)
+           <option class="{{ $state->id }}">{{ $state->name }}</option>
+          @endforeach
          </select>
          <button type="submit" class="flex items-center justify-center px-10 bg-shokka-pink text-white font-normal w-full  rounded border-none transition ease-in hover:bg-shokka-blue-hover">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
               <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
           </svg>
-           <span>
+           <span class="py-3">
               PESQUISAR
            </span>
          </button>
